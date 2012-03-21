fb9935c804da59d0445a1b554308fdad114903f7
Andrew Miller <amiller@dappervision.com>
2012-03-21 16:12:35 -0400

    Added opengl state contextmanagers
    Signed-off-by: Andrew Miller <amiller@dappervision.com>

---
+++ wxpy3d/opengl_state.py
@@ -0,0 +1,32 @@
+from OpenGL.GL import *
+
+from contextlib import contextmanager
+@contextmanager
+def opengl_attrib():
+    try:
+        glPushClientAttrib(GL_ALL_CLIENT_ATTRIB_BITS)
+        glPushAttrib(GL_ALL_ATTRIB_BITS)
+        yield
+    finally:
+        glPopClientAttrib(GL_ALL_CLIENT_ATTRIB_BITS)
+        glPopAttrib(GL_ALL_ATTRIB_BITS)
+
+@contextmanager
+def opengl_matrix():
+    try:
+        glMatrixMode(GL_PROJECTION)
+        glPushMatrix()
+        glMatrixMode(GL_MODELVIEW)
+        glPushMatrix()
+        yield
+    finally:
+        glMatrixMode(GL_PROJECTION)
+        glPopMatrix()
+        glMatrixMode(GL_MODELVIEW)
+        glPopMatrix()
+
+@contextmanager
+def opengl_state():
+    with opengl_attrib():
+        with opengl_matrix():
+            yield
